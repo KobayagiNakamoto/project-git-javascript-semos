d77ccdc7c3d746679dcdd2da4628cf251e2223b3
KobayagiNakamoto <63250177+KobayagiNakamoto@users.noreply.github.com>
2020-10-05 21:39:01 +0200

    Adds file into new branch

--- example.js
+++ example.js
@@ -1,3 +1,4 @@
 console.log("this is ex");
 console.log("aj sho zborish");
 console.log("semos");
+console.log("javaScript")
---
+++ new feature.js
@@ -0,0 +1,2 @@
+console.log("sum od 5 i 6 e", 5 + 6);
+console.log("nov branch uspeshno kreairan");
